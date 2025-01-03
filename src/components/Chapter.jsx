791aab14f347e803ef8a75ebfea88878c7a0dfe8
nayanepreta <nayanepreta01@gmail.com>
2025-01-03 10:26:27 -0300

    Add files via upload

--- src/components/Chapter.jsx
+++ src/components/Chapter.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from 'react';
 import DOMPurify from 'dompurify';
 import chapters from '../control/chapters';
 import Header from '../components/Header';
@@ -6,42 +6,6 @@ import Navigation from './Navigation';
 
 const Chapter = ({ chapterNumber, onNext, goToSummary, goToCover, goToContents }) => {
   const { title: chapterTitle, chap: chapterRoman, img: image, texts: chapterTexts } = chapters[chapterNumber];
-  
-  const [isHeaderVisible, setIsHeaderVisible] = useState(false);
-  const [lastScrollTop, setLastScrollTop] = useState(0);
-  const [lastTouchTime, setLastTouchTime] = useState(0);
-
-  const handleScroll = () => {
-    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (currentScrollTop < lastScrollTop) {
-      setIsHeaderVisible(true);
-    } else {
-      setIsHeaderVisible(false);
-    }
-
-    setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop); 
-  };
-
-  const handleTouch = (e) => {
-    const currentTime = new Date().getTime();
-    const timeDifference = currentTime - lastTouchTime;
-
-    if (timeDifference < 300) { 
-      setIsHeaderVisible(prevState => !prevState);
-    }
-    setLastTouchTime(currentTime); 
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    window.addEventListener("touchstart", handleTouch);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      window.removeEventListener("touchstart", handleTouch);
-    };
-  }, [lastScrollTop, lastTouchTime]);
 
   useEffect(() => {
     const bookTitleT = chapters[0].titulo;
@@ -54,11 +18,12 @@ const Chapter = ({ chapterNumber, onNext, goToSummary, goToCover, goToContents }
 
   return (
     <div className="page chapter_page">
-      {isHeaderVisible && <Header />}
-
-      <span className="chapter_subtitle">Capítulo {chapterRoman}</span>
-      <h2 className="chapter_title">{chapterTitle}</h2>
-      <img className="chapter_img_abertura" src={image} alt="" loading="lazy" />
+      <Header />
+      <div className="chapter_head">
+        <span className="chapter_subtitle">Chapter {chapterRoman}</span>
+        <h2 className="chapter_title">{chapterTitle}</h2>
+        <img className="chapter_img_abertura" src={image} alt="" loading="lazy" />
+      </div>
 
       <div className="chapter_content">
         {chapterTexts.map((paragraph, index) => (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Header = () => {
+  const [isHeaderVisible, setIsHeaderVisible] = useState(false);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [lastTouchTime, setLastTouchTime] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScrollTop < lastScrollTop) {
+      setIsHeaderVisible(true);
+    } else {
+      setIsHeaderVisible(false);
+    }
+
+    setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
+  };
+
+  const handleTouch = (e) => {
+    const currentTime = new Date().getTime();
+    const timeDifference = currentTime - lastTouchTime;
+
+    if (timeDifference < 300) {
+      setIsHeaderVisible((prevState) => !prevState);
+    }
+    setLastTouchTime(currentTime);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('touchstart', handleTouch);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('touchstart', handleTouch);
+    };
+  }, [lastScrollTop, lastTouchTime]);
+
+  if (!isHeaderVisible) return null;
+
   return (
     <div className="header">
-        <div>
-          <button>A+</button>
-          <button>A-</button> 
-        </div>
-        <div>
-          <button>Download</button>
-          <button>Noturno</button>
-        </div>
+      <div>
+        <button>A+</button>
+        <button>A-</button>
+      </div>
+      <div>
+        <button>Download</button>
+        <button>Noturno</button>
+      </div>
     </div>
   );
 };
